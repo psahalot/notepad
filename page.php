f6785acef2eb38e8bc10e0f41983eaae90da1832
Puneet Sahalot <puneetsahalot@gmail.com>
2014-04-17 17:46:16 +0530

    added check for comments and sidebar call

--- page.php
+++ page.php
@@ -31,6 +31,15 @@ get_header(); ?>
 
 			<?php endif; // end have_posts() check ?>
                         
+                        <?php get_sidebar(); ?>
+                                
+                        <?php
+                        // If comments are open or we have at least one comment, load up the comment template
+                        if ( comments_open() || '0' != get_comments_number() ) {
+                                comments_template( '', true );
+                        }
+                        ?>
+
                     </div> <!-- /.main-content -->
                     
 		</div> <!-- /.col.grid_12_of_12 -->
